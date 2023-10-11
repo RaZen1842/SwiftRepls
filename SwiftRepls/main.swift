d4c867549a268faea3a95ad59d50c744d3eb2eae
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2023-10-11 17:12:56 +0100

    Doing 2 Files

--- SwiftRepls/main.swift
+++ SwiftRepls/main.swift
@@ -1,11 +1,81 @@
-//
-//  main.swift
-//  SwiftRepls
-//
-//  Created by Veturi, Shreyas (HJRM) on 11/10/2023.
-//
-
 import Foundation
 
-print("Hello, Shreyas!")
+enum mood {
+  case Happy
+  case Sad
+}
 
+print("Number? ")
+
+let userInput: String = getStringInt()
+
+var digitArray = Array(userInput)
+
+
+var intArray = [Int]()
+
+for digit in digitArray {
+  let tempDigit = String(digit)
+    guard let tempTempDigit = Int(tempDigit) else{
+      print("Invalid Input")
+      break
+    }
+    intArray.append(tempTempDigit)
+}
+
+print(intArray)
+
+var sum = 0
+
+for number in intArray {
+  let temp = number
+  sum = sum + (temp*temp)
+}
+print(sum)
+
+if sum == 1 {
+  print(mood.Happy)
+}
+else if sum == 4 {
+  print(mood.Sad)
+}
+else {
+//LOOP FROM HERE
+  var found = false
+  
+  while found == false {
+  
+    let stringSum = String(sum)
+    
+    var intSumArray = [Int]()
+    
+    let stringSumArray = Array(stringSum)
+    
+    for digit in stringSumArray {
+      let tempDigit = String(digit)
+        guard let tempTempDigit = Int(tempDigit) else{
+          print("Invalid Input")
+          break
+        }
+        intSumArray.append(tempTempDigit)
+    }
+    
+    print(intSumArray)
+  
+    for number in intSumArray {
+      let temp = number
+      sum = sum + (temp*temp)
+    }
+    print(sum)
+
+    if sum == 1 {
+      print(mood.Happy)
+      found = true
+    }
+    else if sum == 4 {
+      print(mood.Sad)
+      found = true
+    }
+  
+  }
+}
---
+++ SwiftRepls/unwrap.swift
@@ -0,0 +1,15 @@
+//
+//  unwrap.swift
+//  SwiftRepls
+//
+//  Created by Veturi, Shreyas (HJRM) on 11/10/2023.
+//
+
+import Foundation
+
+func getStringInt() -> String {
+  guard let input = readLine() else {
+    return ""
+  }
+  return input
+}
